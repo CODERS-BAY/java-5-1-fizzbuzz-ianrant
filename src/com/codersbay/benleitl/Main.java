2d4706442d204db627713cc5435370de07d22566
Ben Leitl <ben.leitl@me.com>
2020-02-12 09:43:39 +0100

    Add functionality :)

--- src/com/codersbay/benleitl/Main.java
+++ src/com/codersbay/benleitl/Main.java
@@ -3,5 +3,25 @@ package com.codersbay.benleitl;
 public class Main {
     public static void main(String[] args) {
 
+        String buzz = "Buzz!";
+        String fizz = "Fizz!";
+        String fizzBuzz = "FizzBuzz!";
+        String number;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 3 != 0) {
+                System.out.println(buzz);
+            } else if (i % 3 == 0 && i % 5 != 0) {
+                System.out.println(fizz);
+            } else if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println(fizzBuzz);
+            } else {
+                number = String.format("%3d", i);
+                System.out.println(number);
+            }
+
+
+        }
+
     }
 }
